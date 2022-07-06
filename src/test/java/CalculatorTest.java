cdde7348eaf1a7dbb5cb1ddd1bfee6a75d51cd25
makhnach12 <nikitamakhno@mail.ru>
2022-07-06 12:05:09 +0300

    Make problems checker

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+import ru.ac.uniyar.mf.makhno.Main;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+    @Test
+    void addition(){
+        String actual = Main.calculate(1,"1", "+", "1");
+        assertEquals("2", actual);
+    }
+
+    @Test
+    void multiplication(){
+        String actual = Main.calculate(1,"1", "*", "1");
+        assertEquals("1", actual);
+    }
+
+    @Test
+    void division(){
+        String actual = Main.calculate(1,"1", "/", "0");
+        assertEquals("0", actual);
+    }
+
+    @Test
+    void frac_plus(){
+        String actual = Main.calculate(2,"1/2", "+", "1/2");
+        assertEquals("1/1", actual);
+    }
+}
