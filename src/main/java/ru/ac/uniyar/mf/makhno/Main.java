0f077511e77ac593f2cdee727a797055c14094c6
makhnach12 <nikitamakhno@mail.ru>
2022-07-07 11:09:04 +0300

    Удалены кейсы в мейне

--- src/main/java/ru/ac/uniyar/mf/makhno/Main.java
+++ src/main/java/ru/ac/uniyar/mf/makhno/Main.java
@@ -1,4 +1,6 @@
 package ru.ac.uniyar.mf.makhno;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -78,93 +80,60 @@ class fraction{
 
 }
 public class Main {
+
+    static void check_parts(String[] parts, fraction frac_1){
+        int num_2;
+        if (parts.length == 2) {
+            num_2 = Integer.parseInt(parts[1]);
+            frac_1.set_denominator(num_2);
+        }
+    }
+
     public static void main(String[] args) {
         ArrayList<fraction> arr = new ArrayList<fraction>();
-        System.out.println("Выберите калькулятор:");
         Scanner in = new Scanner(System.in);
-        int _case = in.nextInt();
-        switch (_case) {
-            case 1:
-                int num_1 = in.nextInt();
-                fraction frac_3 = new fraction(num_1);
-                arr.add(frac_3);
-                String symb = in.next();
-                int num_2 = in.nextInt();
-                fraction frac_4 = new fraction(num_1);
-                arr.add(frac_4);
-                switch (symb) {
-                    case "+":
-                        fraction new_frac_5 = frac_3.sum(frac_4);
-                        new_frac_5.showfrac();
-                        break;
-                    case "-":
-                        fraction new_frac_6 = frac_3.minus(frac_4);
-                        new_frac_6.showfrac();
-                        break;
-                    case "*":
-                        fraction new_frac_7 = frac_3.multiply(frac_4);
-                        new_frac_7.showfrac();
-                        break;
-                    case "/":
-                        if (num_2 == 0) {
-                            System.out.println("Нет решений");
-                            break;
-                        } else {
-                            fraction new_frac_8 = frac_3.div(frac_4);
-                            new_frac_8.showfrac();
-                        }
-                        break;
-                }
+        int num_1 = 1;
+        int num_2 = 1;
+        String frac = in.next();
+        String[] parts = frac.split("/");
+        parts = frac.split("/");
+        int num_3 = Integer.parseInt(parts[0]);
+        fraction frac_1 = new fraction(num_3);
+        check_parts(parts, frac_1);
+        arr.add(frac_1);
+        String symb3 = in.next();
+        frac = in.next();
+        String[] parts2 = frac.split("/");
+        int num_4 = Integer.parseInt(parts2[0]);
+        fraction frac_2 = new fraction(num_4);
+        check_parts(parts2, frac_2);
+        arr.add(frac_2);
+        if (num_1 == 0 || num_2 == 0) {
+            System.out.println("Нет решений");
+            return;
+        }
+        switch (symb3) {
+            case "+":
+                fraction new_frac = frac_1.sum(frac_2);
+                new_frac.showfrac();
                 break;
-            case 2:
-                int num_4 = 1;
-                int num2 = 1;
-                String frac = in.next();
-                String[] parts = frac.split("/");
-                int num1 = Integer.parseInt(parts[0]);
-                fraction frac_1 = new fraction(num1);
-                if (parts.length == 2) {
-                    num2 = Integer.parseInt(parts[1]);
-                    frac_1.set_denominator(num2);
-                }
-                String symb3 = in.next();
-                String frac2 = in.next();
-                String[] parts2 = frac2.split("/");
-                int num_3 = Integer.parseInt(parts2[0]);
-                fraction frac_2 = new fraction(num_3);
-                if (parts2.length == 2) {
-                    num_4 = Integer.parseInt(parts2[1]);
-                    frac_2.set_denominator(num_4);
-                }
-                if (num_4 == 0 || num2 == 0){
-                    System.out.println("Нет решений");
-                    return;
-                }
-                switch (symb3) {
-                    case "+":
-                        fraction new_frac = frac_1.sum(frac_2);
-                        new_frac.showfrac();
-                        break;
-                    case "-":
-                        fraction new_frac_2 = frac_1.minus(frac_2);
-                        new_frac_2.showfrac();
-                        break;
-                    case "*":
-                        fraction new_frac_3 = frac_1.multiply(frac_2);
-                        new_frac_3.showfrac();
-                        break;
-                    case "/":
-                        fraction new_frac_4 = frac_1.div(frac_2);
-                        new_frac_4.showfrac();
-                        break;
-                }
+            case "-":
+                fraction new_frac_2 = frac_1.minus(frac_2);
+                new_frac_2.showfrac();
+                break;
+            case "*":
+                fraction new_frac_3 = frac_1.multiply(frac_2);
+                new_frac_3.showfrac();
+                break;
+            case "/":
+                fraction new_frac_4 = frac_1.div(frac_2);
+                new_frac_4.showfrac();
+                break;
         }
         System.out.println(arr.size());
-
-        if(arr.contains(0)){
+        if (arr.contains(0)) {
             System.out.println("ArrayList contains 0");
-        }
-        else
+        } else
             System.out.println("ArrayList isn't contains 0");
     }
 
